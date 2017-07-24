914444270b9ee800dc933de163266f507994e63e
Divesh Panwar <divesh.panwar@uiit.ac.in>
2017-07-24 21:12:32 +0530

    Few Modifications

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,16 @@
 var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
+var x = document.getElementsByClassName("mySlides");
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
   x[slideIndex-1].style.display = "block";
+};
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
