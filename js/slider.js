71c6873829e634bf7cf5a39eaeffe744d0c9e93b
diveshpanwar <divesh.panwar@gmail.com>
2017-07-25 18:50:59 +0530

    PageSpeed Optimization 3

--- js/slider.js
+++ js/slider.js
@@ -2,13 +2,13 @@ var slideIndex = 1;
 var x = document.getElementsByClassName("mySlides");
 function showDivs(n) {
   var i;
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  if (n > x.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = x.length;}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
   x[slideIndex-1].style.display = "block";
-};
+}
 showDivs(slideIndex);
 
 function plusDivs(n) {
